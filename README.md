cb03f92d9b808320c74d905dbdc779cc315ef09e
leoramos182 <leo.ramos182@hotmail.com>
2021-03-02 20:48:14 -0300

    Aula 01 sobre collections

---
+++ README.md
@@ -0,0 +1,4 @@
+# :facepunch: Aula sobre collections Everis Bootcamp kotlin developer :facepunch:
+
+- Aula 01 : Aprendi varios jeitos de realizar um loop com IntArray (02/03/2021)
+
---
+++ testeIntArray.kt
@@ -0,0 +1,29 @@
+package one.digitalinnovation.collections
+const val LINHA = "*****"
+fun main() {
+    val values = IntArray(5)
+    values[0] = 3
+    values[1] = 5
+    values[2] = 0
+    values[3] = 1
+    values[4] = 2
+
+    for(itens in values){
+        println(itens)
+    }
+    println(LINHA)
+    values.forEach {
+        println(it)
+    }
+    println(LINHA)
+    for(i in values.indices){
+        println(values[i])
+    }
+    println(LINHA)
+    values.sort()
+    for(valor in values){
+        println(valor)
+    }
+
+
+}
