65390435641def507adeda9d7af4afbde33a7fca
leoramos182 <leo.ramos182@hotmail.com>
2021-03-03 17:56:04 -0300

    aula 11 do jether

--- testMapOf.kt
+++ testMapOf.kt
@@ -1,5 +1,13 @@
 package one.digitalinnovation.collections
 
 fun main() {
+    val pair = Pair("Joao",1500.0)
+    val map1 = mapOf(pair)
 
+    map1.forEach { (key, valor) -> println("Chave: $key * Valor: $valor") }
+
+    val map2 = mapOf("Maria" to 1500.0,
+                     "Renato" to 5000.0,
+                     "John" to 3000.0)
+    map2.forEach {(key,valor) -> println("Chave: $key * Valor: $valor") }
 }
